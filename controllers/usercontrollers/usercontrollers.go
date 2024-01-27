5e918fded891b99728ce76d16e9eedb8eb89e8d6
vidiana <vidiana.vidiana98@gmail.com>
2024-01-28 01:02:47 +0700

    fixing user contrroler

--- controllers/usercontrollers/usercontrollers.go
+++ controllers/usercontrollers/usercontrollers.go
@@ -12,18 +12,18 @@ import (
 
 func Index(c *gin.Context) {
 
-	var user []models.User
+	var users []models.User
 
-	models.DB.Find(&user)
-	c.JSON(http.StatusOK, gin.H{"user": user})
+	models.DB.Find(&users)
+	c.JSON(http.StatusOK, gin.H{"users": users})
 
 }
 
 func Show(c *gin.Context) {
-	var user models.User
+	var users models.User
 	id := c.Param("id")
 
-	if err := models.DB.First(&user, id).Error; err != nil {
+	if err := models.DB.First(&users, id).Error; err != nil {
 		switch err {
 		case gorm.ErrRecordNotFound:
 			c.AbortWithStatusJSON(http.StatusNotFound, gin.H{"message": "Data tidak ditemukan"})
@@ -34,32 +34,32 @@ func Show(c *gin.Context) {
 		}
 	}
 
-	c.JSON(http.StatusOK, gin.H{"user": user})
+	c.JSON(http.StatusOK, gin.H{"user": users})
 }
 
 func Create(c *gin.Context) {
 
-	var user models.User
+	var users models.User
 
-	if err := c.ShouldBindJSON(&user); err != nil {
+	if err := c.ShouldBindJSON(&users); err != nil {
 		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": err.Error()})
 		return
 	}
 
-	models.DB.Create(&user)
-	c.JSON(http.StatusOK, gin.H{"user": user})
+	models.DB.Create(&users)
+	c.JSON(http.StatusOK, gin.H{"user": users})
 }
 
 func Update(c *gin.Context) {
-	var user models.User
+	var users models.User
 	id := c.Param("id")
 
-	if err := c.ShouldBindJSON(&user); err != nil {
+	if err := c.ShouldBindJSON(&users); err != nil {
 		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": err.Error()})
 		return
 	}
 
-	if models.DB.Model(&user).Where("id = ?", id).Updates(&user).RowsAffected == 0 {
+	if models.DB.Model(&users).Where("id = ?", id).Updates(&users).RowsAffected == 0 {
 		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": "tidak dapat mengupdate product"})
 		return
 	}
@@ -70,7 +70,7 @@ func Update(c *gin.Context) {
 
 func Delete(c *gin.Context) {
 
-	var user models.User
+	var users models.User
 
 	var input struct {
 		Id json.Number
@@ -82,7 +82,7 @@ func Delete(c *gin.Context) {
 	}
 
 	id, _ := input.Id.Int64()
-	if models.DB.Delete(&user, id).RowsAffected == 0 {
+	if models.DB.Delete(&users, id).RowsAffected == 0 {
 		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": "Tidak dapat menghapus product"})
 		return
 	}
--- models/user.go
+++ models/user.go
@@ -2,8 +2,8 @@ package models
 
 type User struct {
 	Id       int64  `gorm:"primaryKey" json:"id"`
-	username string `gorm:"type:varchar(100)" json:"username"`
-	password string `gorm:"type:varchar(100)" json:"password"`
-	email    string `gorm:"type:varchar(100)" json:"email"`
-	photo    []byte `gorm:"type:binary(255)" json:"photo"`
+	Username string `gorm:"type:varchar(100)" json:"username"`
+	Password string `gorm:"type:varchar(100)" json:"password"`
+	Email    string `gorm:"type:varchar(100)" json:"email"`
+	Photo    []byte `gorm:"type:binary(255)" json:"photo"`
 }
