845ebdefd1d0ca315165e28105c3ce80f1e42be5
vidiana <vidiana.vidiana98@gmail.com>
2024-01-28 00:44:39 +0700

    add user controller update

---
+++ controllers/usercontrollers/usercontrollers.go
@@ -0,0 +1,91 @@
+package usercontrollers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/vidiana0101/task-5-pbi-bptns-VIDIANA/models"
+	"gorm.io/gorm"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Index(c *gin.Context) {
+
+	var user []models.User
+
+	models.DB.Find(&user)
+	c.JSON(http.StatusOK, gin.H{"user": user})
+
+}
+
+func Show(c *gin.Context) {
+	var user models.User
+	id := c.Param("id")
+
+	if err := models.DB.First(&user, id).Error; err != nil {
+		switch err {
+		case gorm.ErrRecordNotFound:
+			c.AbortWithStatusJSON(http.StatusNotFound, gin.H{"message": "Data tidak ditemukan"})
+			return
+		default:
+			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
+			return
+		}
+	}
+
+	c.JSON(http.StatusOK, gin.H{"user": user})
+}
+
+func Create(c *gin.Context) {
+
+	var user models.User
+
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+		return
+	}
+
+	models.DB.Create(&user)
+	c.JSON(http.StatusOK, gin.H{"user": user})
+}
+
+func Update(c *gin.Context) {
+	var user models.User
+	id := c.Param("id")
+
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+		return
+	}
+
+	if models.DB.Model(&user).Where("id = ?", id).Updates(&user).RowsAffected == 0 {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": "tidak dapat mengupdate product"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Data berhasil diperbarui"})
+
+}
+
+func Delete(c *gin.Context) {
+
+	var user models.User
+
+	var input struct {
+		Id json.Number
+	}
+
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+		return
+	}
+
+	id, _ := input.Id.Int64()
+	if models.DB.Delete(&user, id).RowsAffected == 0 {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": "Tidak dapat menghapus product"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Data berhasil dihapus"})
+}
--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/vidiana0101/task-5-pbi-bptns-VIDIANA/controllers/usercontrollers"
+	"github.com/vidiana0101/task-5-pbi-bptns-VIDIANA/models"
+)
 
+func main() {
+	r := gin.Default()
+	models.ConnectDatabase()
+
+	r.GET("/api/user", usercontrollers.Index)
+	r.GET("/api/user/:id", usercontrollers.Show)
+	r.POST("/api/user", usercontrollers.Create)
+	r.PUT("/api/user/:id", usercontrollers.Update)
+	r.DELETE("/api/user", usercontrollers.Delete)
+
+	r.Run()
 }
